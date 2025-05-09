f8ab1f2ef723d36db013ebf967039f2e4fb80f8f
bhushansalunke <bhushansalunke55@gmail.com>
2025-05-09 22:16:18 +0530

    Add Welcome message to DITISS

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,6 @@ public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, Mr. Bhushan Salunke!");
 	System.out.println("Welcome to C-DAC");
+        System.out.println("Welcome to DITISS");
     }
 }
