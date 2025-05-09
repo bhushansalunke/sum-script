b671c23eb0e6a6b07368d1a233b12a42601a88be
bhushansalunke <bhushansalunke55@gmail.com>
2025-05-09 22:11:34 +0530

    Add Welcome message to HelloStudent

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,5 +1,6 @@
 public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, Mr. Bhushan Salunke!");
+	System.out.println("Welcome to C-DAC");
     }
 }
