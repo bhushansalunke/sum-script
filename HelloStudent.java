543118f589da79414c955ba6b7f4e9a42da575a6
bhushansalunke <bhushansalunke55@gmail.com>
2025-05-09 23:19:51 +0530

    Test 2

--- HelloStudent.java
+++ HelloStudent.java
@@ -4,5 +4,6 @@ public class HelloStudent {
 	System.out.println("Welcome to C-DAC");
         System.out.println("Welcome to DITISS");
 	System.out.println("Test 1");
+	System.out.println("Test 2");
     }
 }
