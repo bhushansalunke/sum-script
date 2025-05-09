6581791c970b0eb4424a844f53f4deb010983659
bhushansalunke <bhushansalunke55@gmail.com>
2025-05-09 21:56:22 +0530

    Add HelloStudent Java program

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,7 +1,5 @@
-cat > HelloStudent.java << 'EOF'
 public class HelloStudent {
     public static void main(String[] args) {
-        System.out.println("Hello, Bhushan Salunke!");
+        System.out.println("Hello, Mr. Bhushan Salunke!");
     }
 }
-EOF
