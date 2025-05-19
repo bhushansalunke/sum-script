c7d4220d44b91c80b85fc0c7bb2405b23dfd2a1c
Bhushan Salunke <41441170+bhushansalunke@users.noreply.github.com>
2025-05-19 03:29:03 -0700

    Update HelloStudent.java

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,8 +1,7 @@
 public class HelloStudent {
     public static void main(String[] args) {
-        System.out.println("Hello, Mr. Bhushan Salunke CISO!");
-        System.out.println("Welcome to C-DAC");
-        System.out.println("Welcome to DITISS");
+        System.out.println("Hello World");
+        
     }
 }
 
